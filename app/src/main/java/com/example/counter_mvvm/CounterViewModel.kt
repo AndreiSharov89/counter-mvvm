f2f23feae896b7e804b188f1c2d17a38504ddcfb
Andrei <sharov89@gmail.com>
2025-10-07 11:49:06 +0300

    3/7

--- app/src/main/java/com/example/counter_mvvm/CounterViewModel.kt
+++ app/src/main/java/com/example/counter_mvvm/CounterViewModel.kt
@@ -1,14 +1,35 @@
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.counter_mvvm.R
 
-class CounterViewModel(private val startValue: Int) : ViewModel() {
+
+class CounterViewModel(
+    private val startValue: Int,
+    private val prefix: String
+) : ViewModel() {
 
     private var counterValue = startValue
 
     fun getCounter(): String {
-        return counterValue.toString()
+        return prefix + " " + counterValue
     }
 
     fun incrementCounter() {
         counterValue++
     }
+
+    companion object {
+        fun getFactory(value: Int): ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val app = (this[APPLICATION_KEY] as Application)
+                val text = app.getString(R.string.counter_prefix)
+                CounterViewModel(value, text)
+            }
+        }
+    }
+
 } 
--- app/src/main/java/com/example/counter_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/counter_mvvm/MainActivity.kt
@@ -4,13 +4,13 @@ import CounterViewModel
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.counter_mvvm.R
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: CounterViewModel by viewModels()
+    private lateinit var viewModel: CounterViewModel
 
     private lateinit var counter: TextView
     private lateinit var button: Button
@@ -18,6 +18,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startValue = 7
+
+        viewModel = ViewModelProvider(
+            this,
+            CounterViewModel.getFactory(startValue)
+        ).get(CounterViewModel::class.java)
 
         counter = findViewById(R.id.counter)
         button = findViewById(R.id.button)
